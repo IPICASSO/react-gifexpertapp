eb0cdd47888166fcfa3d6ce926f7d569b5319876
IPICASSO <63215008+IPICASSO@users.noreply.github.com>
2021-06-22 22:12:36 -0300

    First commit

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GiftExpertApp = () => {
+
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    return (
+        <>
+            <h2>GiftExpertApp</h2>
+            <AddCategory setCategories={setCategories} categories={categories}/>
+            <hr />
+        
+            <ol>
+                {
+                    categories.map(category => ( 
+                        <GifGrid
+                            key={category} 
+                            category={category}
+                        />
+                    ))
+                }
+            </ol>
+
+        </>
+    );
+
+};
+
+export { GiftExpertApp };
